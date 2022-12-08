70238ba0c608c356d91c321c2cb3698edd0f06f5
Jean-Philippe Bousselin <ssj@foyer.lu>
2022-12-08 12:01:08 +0100

    day01-2

--- jeanfi/day01.js
+++ jeanfi/day01.js
@@ -1,1 +1,17 @@
-let transform = (input) => Math.max(...input.match(/([0-9]+\n)*/g).filter(x => x != "").map(x => x.split("\n").slice(0, -1).map(x => parseInt(x)).reduce((a,c) => a+c, 0)))
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+let transform = (input) => 
+  input.match(/([0-9]+\n)*/g)
+        .filter(x => x != "")
+        .map(x => 
+          x.split("\n")
+          .slice(0, -1)
+          .map(x => parseInt(x))
+          .reduce((a,c) => a+c, 0) 
+        )
+        .sort(compareNumbers)
+        .reverse()
+        .slice(0, 3)
+        .reduce((a,c) => a+c, 0) 
